a338843e99e2aea2aadf1bb75450168687f6dff4
Samvel-c <63611105+Samvel-c@users.noreply.github.com>
2020-07-21 14:20:07 +0400

    fvb

--- index.js
+++ index.js
@@ -4,13 +4,35 @@
 // 	console.log(data)
 // }
 // foo()import { setImg,URL,Prof } from './helpers.js';
-import { imgLog } from "./main.js";
+const Prof = ('https://github.com')
+const setImg = (id , imgSrc) => {
+	document.getElementById(id).src = imgSrc  
+}
+	const imgLog = (imgCrs, logName) => {
+		const img = document.createElement("img");
+		img.src = imgCrs;
+		const imgC = document.createElement("div");
+		imgC.classList.add("img");
+		img.style.width = "50%";
+		imgC.appendChild(img);
+		const login = document.createElement("div");
+		login.classList.add("log");
+		login.innerHTML = logName;
+		const div = document.createElement("div");
+		div.classList.add("div");
+		div.appendChild(imgC);
+		div.appendChild(login);
+		document.querySelectorAll(".sec").forEach((e) => {
+		  e.appendChild(div);
+		});
+	  };
+	  
 
 const searchInput = document.getElementById('input');
 const search = document.getElementById('search');
 async function api() {
     try {
-      const resp = await fetch(URL);
+      const resp = await fetch('https://api.github.com/users');
       const data = await resp.json();
       data.map((data) => {
         imgLog(data.avatar_url, data.login);
@@ -22,10 +44,9 @@ async function api() {
   }
   api();
 const UserApi = () => {
-    const loading = document.getElementById('loading')
-    loading.innerHTML = 'Loading.....'
     
-    fetch(`${URL}/${searchInput.value}`)
+    
+    fetch(`${'https://api.github.com/users'}/${searchInput.value}`)
     .then(resp => {
         console.log(resp)
         return resp.json()
